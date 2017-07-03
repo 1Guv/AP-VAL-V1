6c2dfc063315d017e95469497ea8c62588320c22
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-07-03 19:43:17 +0100

    added 2 questions and start, next and back buttons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-console.log('Yo');
+
+
+$(document).ready(function(){
+
+
+  console.log('Yo');
+
+  var $startButton = $('.start-button');
+  var $nextButton = $('.next-button');
+  var $backButton = $('.back-button');
+
+  var $q = $('.question');
+
+  function questionOne() {
+    $q.html('Please state the exact number plate with the correct spacing?');
+  }
+
+  function questionTwo() {
+    $q.html('Please state the plate in ENGLISH or PUNJABI words with spaces?')
+  }
+
+  function questionThree() {
+    $q.html('Please state the plate in ENGLISH or PUNJABI words with spaces?')
+  }
+
+  $startButton.click(function() {
+    questionOne();
+    $startButton.html('START AGAIN');
+
+    $startButton.click(function() {
+      questionOne();
+    });
+
+  $nextButton.click(function(){
+    questionTwo();
+  });
+
+  $backButton.click(function(){
+    questionOne();
+  });
+
+  });
+
+
+
+
+});
