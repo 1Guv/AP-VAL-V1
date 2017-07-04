366a59ab946f104fec9fbd4366fceef40730c289
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-07-04 22:03:35 +0100

    added all questions

--- js/app.js
+++ js/app.js
@@ -44,6 +44,52 @@ $(document).ready(function(){
     $q.html('Q6: What are the total amount of characters in the plate?')
   }
 
+  function questionSeven() {
+    counter = 7;
+    $q.html('Q7: How many digits?')
+  }
+
+  function questionEight() {
+    counter = 8;
+    $q.html('Q8: How many letters?')
+  }
+
+  function questionNine() {
+    counter = 9;
+    $q.html('Q9: What is the plate type eg CURRENT, PREFIX, SUFFIX or DATELESS?')
+  }
+
+  function questionTen() {
+    counter = 10;
+    $q.html('Q10: What is the age of the plate?')
+  }
+
+  function questionElleven() {
+    counter = 11;
+    $q.html('Q11: How many spaces between words or numbers? (1,2,3 etc or none)')
+  }
+
+  function questionTwelve() {
+    counter = 12;
+    $q.html('Q12: Do any of the (INTEGER) letters need to be modified?')
+  }
+
+  function questionThirteen() {
+    counter = 13;
+    $q.html('Q13: Do any of the 1 numbers need to be modified?')
+  }
+
+  function questionFourteen() {
+    counter = 14;
+    $q.html('Q14: Do any of the 1 numbers need to be modified?')
+  }
+
+  function questionFifteen() {
+    counter = 15;
+    $q.html('Q15: Do any of the (INTEGER) numbers need to be modified?')
+  }
+
+
   $startButton.click(function() {
     questionOne();
     $startButton.html('START AGAIN');
@@ -96,6 +142,33 @@ $(document).ready(function(){
         break;
       case 6:
         questionSix();
+        break;
+      case 7:
+        questionSeven();
+        break;
+      case 8:
+        questionEight();
+        break;
+      case 9:
+        questionNine();
+        break;
+      case 10:
+        questionTen();
+        break;
+      case 11:
+        questionElleven();
+        break;
+      case 12:
+        questionTwelve();
+        break;
+      case 13:
+        questionThirteen();
+        break;
+      case 14:
+        questionFourteen();
+        break;
+      case 15:
+        questionFifteen();
         break;
     }
   }
