2dbc61c8956988c46045c652ffc6758ba62b99ae
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-07-21 14:07:09 +0100

    created basic design showing the results for each question

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
 
 
@@ -8,6 +6,7 @@ $(document).ready(function(){
   var $startButton = $('.start-button');
   var $nextButton = $('.next-button');
   var $backButton = $('.back-button');
+  var $submitButton = $('.submit-button');
 
   var counter = 0;
   var next = 0;
@@ -15,19 +14,164 @@ $(document).ready(function(){
 
   var $q = $('.question');
   var $qexample = $('.example');
+  var q1Input;
+  var q2Input;
+  var q3Input;
+  var q4Input;
+
+  // for each question the submit button is clicked
+  $submitButton.click(function() {
+
+    switch (counter) {
+      case 1:
+        q1Input = $('.UserInput').val();
+        q1Input = q1Input.toUpperCase();
+        console.log(q1Input);
+        $('.Q1UserPlate').html("Q1 - Plate: " + q1Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 2:
+        q2Input = $('.UserInput').val();
+        q2Input = q2Input.toUpperCase();
+        console.log(q2Input);
+        // remove the text
+
+        $('.Q2UserPlateActual').html("Q2 - Actual Plate: " + q2Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 3:
+        q3Input = $('.UserInput').val();
+        q3Input = q3Input.toUpperCase();
+        console.log(q3Input);
+        $('.Q3PlateCharacters').html("Q3 - Characters: " + q3Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 4:
+        q4Input = $('.UserInput').val();
+        q4Input = q4Input.toUpperCase();
+        console.log(q4Input);
+        $('.Q4DuplicateChars').html("Q4 - Duplicate characters: " + q4Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 5:
+        q5Input = $('.UserInput').val();
+        q5Input = q5Input.toUpperCase();
+        console.log(q5Input);
+        $('.Q5GoodMatch').html("Q5 - Good match: " + q5Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 6:
+        q6Input = $('.UserInput').val();
+        q6Input = q6Input.toUpperCase();
+        console.log(q6Input);
+        $('.Q6TotChars').html("Q6 - Total characters: " + q6Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 7:
+        q7Input = $('.UserInput').val();
+        q7Input = q7Input.toUpperCase();
+        console.log(q7Input);
+        $('.Q7Digits').html("Q7 - Total digits: " + q7Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 8:
+        q8Input = $('.UserInput').val();
+        q8Input = q8Input.toUpperCase();
+        console.log(q8Input);
+        $('.Q8Letters').html("Q8 - Total letters: " + q8Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 9:
+        q9Input = $('.UserInput').val();
+        q9Input = q9Input.toUpperCase();
+        console.log(q9Input);
+        $('.Q9Type').html("Q9 - Type: " + q9Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 10:
+        q10Input = $('.UserInput').val();
+        q10Input = q10Input.toUpperCase();
+        console.log(q10Input);
+        $('.Q10Age').html("Q10 - Plate age: " + q10Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 11:
+        q11Input = $('.UserInput').val();
+        q11Input = q11Input.toUpperCase();
+        console.log(q11Input);
+        $('.Q11Spaces').html("Q11 - Plate spaces: " + q11Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 12:
+        q12Input = $('.UserInput').val();
+        q12Input = q12Input.toUpperCase();
+        console.log(q12Input);
+        $('.Q12LetterModify').html("Q12 - Letter modifications: " + q12Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+      case 13:
+        q13Input = $('.UserInput').val();
+        q13Input = q13Input.toUpperCase();
+        console.log(q13Input);
+        $('.Q13DigitModify').html("Q13 - Digit modifications: " + q13Input);
+        $('.UserInput').val('');
+        console.log(counter);
+        counter = counter + 1;
+        backOrNext();
+        break;
+    }
+  });
 
   function questionOne() {
     counter = 1;
+
     $q.html('Q1: Please state the exact number plate with the correct spacing?');
-    $qexample.html('Example of spacing for most plates: [1A], [A1], [1 AB], [AB 1], [1 ABC], [ABC 1], [ABC 123D], [A123 BCD] or [AB51 CDE]');
-    
-    // generate plate for viewing
+    // $qexample.html('Example of spacing for most plates: [1A], [A1], [1 AB], [AB 1], [1 ABC], [ABC 1], [ABC 123D], [A123 BCD] or [AB51 CDE]');
+    $qexample.html('Example: For the purpose of continuity I will use G1 JVS has an example');
 
   }
 
   function questionTwo() {
     counter = 2;
     $q.html('Q2: Please state the plate in ENGLISH or PUNJABI words with spaces?')
+    $qexample.html('Example: GUVS');
+
   }
 
   function questionThree() {
@@ -77,22 +221,12 @@ $(document).ready(function(){
 
   function questionTwelve() {
     counter = 12;
-    $q.html('Q12: Do any of the (INTEGER) letters need to be modified?')
+    $q.html('Q12: Do any of the (CHARACTERS) letters need to be modified?')
   }
 
   function questionThirteen() {
     counter = 13;
-    $q.html('Q13: Do any of the 1 numbers need to be modified?')
-  }
-
-  function questionFourteen() {
-    counter = 14;
-    $q.html('Q14: Do any of the 1 numbers need to be modified?')
-  }
-
-  function questionFifteen() {
-    counter = 15;
-    $q.html('Q15: Do any of the (INTEGER) numbers need to be modified?')
+    $q.html('Q13: Do any of the (INTEGER) numbers need to be modified?')
   }
 
 
@@ -101,6 +235,7 @@ $(document).ready(function(){
     $startButton.html('START AGAIN');
 
     $startButton.click(function() {
+      counter = 0;
       questionOne();
     });
   });
@@ -169,12 +304,6 @@ $(document).ready(function(){
         break;
       case 13:
         questionThirteen();
-        break;
-      case 14:
-        questionFourteen();
-        break;
-      case 15:
-        questionFifteen();
         break;
     }
   }
